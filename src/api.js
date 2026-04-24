bf74860d1d7330b7ad9c2e1b26ff9c629451010e
ikhtiar sabilla firdaus <ikhtiarsabillafirdaus@gmail.com>
2026-04-24 16:41:24 +0700

    konfigurasi api

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'msmbackend-production-765b.up.railway.app', // Sesuaikan dengan port r.Run di main.go
+  baseURL: 'https://msmbackend-production-765b.up.railway.app', // Sesuaikan dengan port r.Run di main.go
 });
