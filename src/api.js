dd693e387ea08ef56cfc45f72522b5e01f08113a
ikhtiar sabilla firdaus <ikhtiarsabillafirdaus@gmail.com>
2026-04-26 22:37:39 +0700

    fix: syntax error in api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_URL = import.meta.VITE_API_URL || 'http://localhost:8083';
 
 const api = axios.create({
-  baseURL = API_URL,
+  baseURL: API_URL,
 });
 
 export default api;
