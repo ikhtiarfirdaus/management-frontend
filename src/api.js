02081ac9d8eea4b50786e364f6d75b562552379e
ikhtiar sabilla firdaus <ikhtiarsabillafirdaus@gmail.com>
2026-04-26 22:42:25 +0700

    fix: export api properly

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API_URL = import.meta.VITE_API_URL || 'http://localhost:8083';
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: API_URL,
 });
 
